d5de095744763d64863b73d35bb652a819a96ccd
Kauan <kauan.nascimento@escolar.ifrn.edu.br>
2023-11-21 18:51:02 -0300

    commit

--- motos/urls.py
+++ motos/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('catalogo/', MotoListView.as_view(), name='catalogo'),
     path('cadastrar/', MotoCreateView.as_view(), name='cadastrar_moto'),
     path('cadastrar/marca/', CadastrarMarcaView.as_view(), name='cadastrar_marca'),
+    path('visualizar_motos/', Moto2ListView.as_view(), name='visualizar_motos'),
+    path('update/<int:pk>/', MotoUpdateView.as_view(), name="editar"),
+    path('remover_motos/<int:pk>/', MotoDeleteView.as_view(), name="remover_motos"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- motos/views.py
+++ motos/views.py
@@ -4,18 +4,41 @@ from .forms import MotoForm, MarcaForm
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from django.contrib import messages
+from django.contrib.messages import views
 
-class MotoCreateView(LoginRequiredMixin, CreateView):
+class MotoCreateView(generic.CreateView):
     model = Moto
     form_class = MotoForm
     template_name = 'cadastrar_moto.html'
     success_url = reverse_lazy('motos:catalogo')
 
-class MotoListView(ListView):
+class MotoListView(generic.ListView):
     model = Moto
     template_name = 'catalogo.html'
 
-class CadastrarMarcaView(LoginRequiredMixin, CreateView):
+class Moto2ListView(generic.ListView):
+    model = Moto
+    template_name = "visualizar_motos.html"
+    paginate_by = 4
+
+class MotoUpdateView(generic.UpdateView):
+    model = Moto
+    form_class = MotoForm
+    success_url = reverse_lazy("motos:visualizar_motos")
+    template_name = "cadastrar_moto.html"
+   
+    def form_valid(self, form):
+        messages.success(self.request, "Cadastro Atualizado!")
+        return super().form_valid(form)
+    
+class MotoDeleteView(views.SuccessMessageMixin,generic.DeleteView):
+    model = Moto
+    success_url = reverse_lazy("motos:visualizar_motos")
+    success_message = "Cadastro deletado!"
+
+class CadastrarMarcaView(generic.CreateView):
     model = Marca
     form_class = MarcaForm
     template_name = 'cadastrar_marca.html'
